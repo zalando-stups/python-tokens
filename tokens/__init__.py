98ca964921ce4087234d333b94cf27cee0112773
Henning Jacobs <henning@jacobs1.de>
2015-07-23 11:03:55 +0200

    add info logging

--- tokens/__init__.py
+++ tokens/__init__.py
@@ -1,7 +1,10 @@
 import json
+import logging
 import os
 import requests
 import time
+
+logger = logging.getLogger('tokens')
 
 ONE_YEAR = 3600*24*365
 
@@ -15,6 +18,7 @@ def init_fixed_tokens_from_env():
     env_val = os.environ.get('OAUTH2_ACCESS_TOKENS', '')
     for part in env_val.split(','):
         key, sep, val = part.partition('=')
+        logger.info('Using fixed access token "%s"..', key)
         TOKENS[key] = {'access_token': val, 'expires_at': time.time() + ONE_YEAR}
 
 
@@ -32,29 +36,37 @@ def start():
     pass
 
 
+def refresh(token_name):
+    logger.info('Refreshing access token "%s"..', token_name)
+    token = TOKENS[token_name]
+    path = CONFIG['dir']
+    with open(os.path.join(path, 'user.json')) as fd:
+        user_data = json.load(fd)
+    with open(os.path.join(path, 'client.json')) as fd:
+        client_data = json.load(fd)
+    body = {
+            'grant_type': 'password',
+            'username': user_data.get('application_username'),
+            'password': user_data.get('application_password'),
+            'scope': ' '.join(token['scopes'])
+            }
+
+    r = requests.post(CONFIG['url'], data=body,
+                      auth=(client_data.get('client_id'), client_data.get('client_secret')))
+    r.raise_for_status()
+    data = r.json()
+    token['data'] = data
+    token['expires_at'] = time.time() + data.get('expires_in')
+    token['access_token'] = data.get('access_token')
+    return token
+
+
 def get(token_name):
     token = TOKENS[token_name]
     access_token = token.get('access_token')
+    # TODO: remove hardcoded time value (20 minutes)
     if not access_token or time.time() > token['expires_at'] - 20*60:
-        path = CONFIG['dir']
-        with open(os.path.join(path, 'user.json')) as fd:
-            user_data = json.load(fd)
-        with open(os.path.join(path, 'client.json')) as fd:
-            client_data = json.load(fd)
-        body = {
-                'grant_type': 'password',
-                'username': user_data.get('application_username'),
-                'password': user_data.get('application_password'),
-                'scope': ' '.join(token['scopes'])
-                }
-
-        r = requests.post(CONFIG['url'], data=body,
-                          auth=(client_data.get('client_id'), client_data.get('client_secret')))
-        r.raise_for_status()
-        data = r.json()
-        token['data'] = data
-        token['expires_at'] = time.time() + data.get('expires_in')
-        token['access_token'] = data.get('access_token')
+        token = refresh(token_name)
 
     access_token = token.get('access_token')
     return access_token
