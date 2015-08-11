819160db3e905456855c5998f1be42a18dec6956
Henning Jacobs <henning@jacobs1.de>
2015-08-11 17:01:20 +0200

    fix log line and add helpful message

--- tokens/__init__.py
+++ tokens/__init__.py
@@ -9,14 +9,14 @@ logger = logging.getLogger('tokens')
 ONE_YEAR = 3600*24*365
 
 CONFIG = {'url': os.environ.get('OAUTH_ACCESS_TOKEN_URL'),
-          'dir': os.environ.get('CREDENTIALS_DIR')}
+          'dir': os.environ.get('CREDENTIALS_DIR', '')}
 
 TOKENS = {}
 
 
 def init_fixed_tokens_from_env():
     env_val = os.environ.get('OAUTH2_ACCESS_TOKENS', '')
-    for part in env_val.split(','):
+    for part in filter(None, env_val.split(',')):
         key, sep, val = part.partition('=')
         logger.info('Using fixed access token "%s"..', key)
         TOKENS[key] = {'access_token': val, 'expires_at': time.time() + ONE_YEAR}
@@ -40,18 +40,22 @@ def refresh(token_name):
     logger.info('Refreshing access token "%s"..', token_name)
     token = TOKENS[token_name]
     path = CONFIG['dir']
+    url = CONFIG['url']
+
+    if not url:
+        raise Exception('Missing OAuth access token URL. ' +
+                        'Either set OAUTH_ACCESS_TOKEN_URL or use tokens.configure(url=..).')
+
     with open(os.path.join(path, 'user.json')) as fd:
         user_data = json.load(fd)
     with open(os.path.join(path, 'client.json')) as fd:
         client_data = json.load(fd)
-    body = {
-            'grant_type': 'password',
+    body = {'grant_type': 'password',
             'username': user_data.get('application_username'),
             'password': user_data.get('application_password'),
-            'scope': ' '.join(token['scopes'])
-            }
+            'scope': ' '.join(token['scopes'])}
 
-    r = requests.post(CONFIG['url'], data=body,
+    r = requests.post(url, data=body,
                       auth=(client_data.get('client_id'), client_data.get('client_secret')))
     r.raise_for_status()
     data = r.json()
