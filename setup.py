7d4440426b10566f65ea4852b2bea3ff451385ce
Henning Jacobs <henning@jacobs1.de>
2015-08-19 10:47:06 +0200

    Bumped version to 0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-__version__ = '0.6'
+__version__ = '0.7'
 
 
 class PyTest(TestCommand):
