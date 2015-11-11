7a607b0359f1ebfd89667d49f1f3e2883f2a64d0
Henning Jacobs <henning@jacobs1.de>
2015-11-11 14:31:33 +0100

    unify release.sh

--- release.sh
+++ release.sh
@@ -13,16 +13,22 @@ git --version
 version=$1
 
 sed -i "s/__version__ = .*/__version__ = '${version}'/" */__init__.py
-python3 setup.py clean
-python3 setup.py test
-python3 setup.py flake8
 
-git add */__init__.py
+# Do not tag/push on Go CD
+if [ -z "$GO_PIPELINE_LABEL" ]; then
+    python3 setup.py clean
+    python3 setup.py test
+    python3 setup.py flake8
 
-git commit -m "Bumped version to $version"
-git push
+    git add */__init__.py
+
+    git commit -m "Bumped version to $version"
+    git push
+fi
 
 python3 setup.py sdist bdist_wheel upload
 
-git tag ${version}
-git push --tags
+if [ -z "$GO_PIPELINE_LABEL" ]; then
+    git tag ${version}
+    git push --tags
+fi
